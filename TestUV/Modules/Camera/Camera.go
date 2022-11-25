7356e52bcfc2e1b77ba5b3b004054565e376d793
Mhadhbi Issam <95243282+MhadhbiXissam@users.noreply.github.com>
2022-11-25 21:05:23 +0100

    Add files via upload

---
+++ TestUV/Modules/Camera/Camera.go
@@ -0,0 +1,15 @@
+components {
+  id: "script"
+  component: "/Modules/Camera/Camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
